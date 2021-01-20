63812e37139c6d0164c5e38ff83f1c9485ea9c84
suyeon <tndus7644@gmail.com>
2021-01-20 22:34:54 +0900

    initcommit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import {GlobalStyle} from "./Styled/Common.Styled";
+
+const App = () => {
+
+    return(
+        <Container>
+            <GlobalStyle/>
+            App
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
+
+
+
+export default App;
